ac10764186b41ca9ef5d4b36cb2dbf9039bf928b
Md-Sakibul-Islam <sakibul.diu@gmail.com>
2024-08-22 19:20:57 +0600

    keyboard event done

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,18 +22,35 @@ $(document).ready(function () {
     $("#showConText").html("Wow You Change The Context !");
   });
 
-
+//   mouse enter event 
   $('#enterBtn').on('mouseenter',function(){
     $(this).css('background-color','salmon')
     $(this).css('color','white')
     $(this).css('transition','0.3s')
   });
 
+  //   mouse leave event 
 
   $('#enterBtn').on('mouseleave',function(){
     $(this).css('background-color','white')
     $(this).css('color','salmon')
     $(this).css('transition','0.3s')
+  });
+
+
+   //   keyboard event 'keypress' 'keydown' almost same 
+  $('body').on('keypress',function(){
+    $('#color-plate').css('height','50px');
+    $('#color-plate').css('background-color','purple');
+    $('#color-plate').css('border-radius','10px');
+  });
+ 
+   //   keyboard event 'keyup' 
+  $('body').on('keyup',function(){
+    $('#circle').css('height','60px');
+    $('#circle').css('width','60px');
+    $('#circle').css('background-color','red');
+    $('#circle').css('border-radius','50%');
   });
  
 });
